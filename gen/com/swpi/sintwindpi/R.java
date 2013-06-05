3b3e4ad2b798c106369de92e31f4858f50f20e4e
Tony <tony.tarsi@gmail.com>
2013-06-05 10:02:44 +0200

    1.6

--- gen/com/swpi/sintwindpi/R.java
+++ gen/com/swpi/sintwindpi/R.java
@@ -30,8 +30,13 @@ public final class R {
     public static final class drawable {
         public static final int ic_checkbox=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int ic_menu_wind=0x7f020002;
-        public static final int ic_swpi=0x7f020003;
+        public static final int ic_menu_data=0x7f020002;
+        public static final int ic_menu_stations=0x7f020003;
+        public static final int ic_menu_tel=0x7f020004;
+        public static final int ic_menu_web=0x7f020005;
+        public static final int ic_menu_webcam=0x7f020006;
+        public static final int ic_menu_wind=0x7f020007;
+        public static final int ic_swpi=0x7f020008;
     }
     public static final class id {
         public static final int ID=0x7f090003;
